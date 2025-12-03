f517de0745a02ee8261cdbd047b8d34b142dba64
VETLE-PC\Vetle <vetleskag@gmail.com>
2025-12-03 20:34:51 +0100

    sanity should now be deployed along with the nextjs app

--- gamedevsyndicate-nextjs/sanity/sanity.config.ts
+++ gamedevsyndicate-nextjs/sanity/sanity.config.ts
@@ -1,13 +1,31 @@
-// import { defineConfig } from 'sanity';
-// import { deskTool } from 'sanity/desk';
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {presentationTool} from 'sanity/presentation'
+import {schemaTypes} from '../../gamedevsyndicate/schemaTypes'
 
-// export default defineConfig({
-//   name: 'default',
-//   title: 'Gamedev Syndicate Content',
-//   projectId: 'your_project_id', // TODO: Replace with your actual Sanity project ID
-//   dataset: 'production',
-//   plugins: [deskTool()],
-//   schema: {
-//     types: [], // Add your schema types here
-//   },
-// });
+export default defineConfig({
+  name: 'default',
+  title: 'Gamedevsyndicate',
+
+  projectId: 'iu8qgjyf',
+  dataset: process.env.NODE_ENV === 'development' ? 'dev' : 'production',
+  basePath: '/studio',
+
+  plugins: [
+    structureTool(), 
+    visionTool(), 
+    presentationTool({
+      previewUrl: {
+        origin: typeof window !== 'undefined' && window.location.origin || 'http://localhost:3000',
+        previewMode: {
+          enable: '/api/draft',
+        },
+      },
+    })
+  ],
+
+  schema: {
+    types: schemaTypes as any,
+  },
+})
